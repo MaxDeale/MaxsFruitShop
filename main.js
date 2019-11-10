4d2258a74201c7821f8f21bd422eb0dafb88bed5
MaxDeale <46649047+MaxDeale@users.noreply.github.com>
2019-11-10 23:13:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+                // When the user clicks on <div>, open the popup
+                function popUp() {
+                    var popup = document.getElementById("myPopup");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp2() {
+                    var popup = document.getElementById("myPopup2");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp3() {
+                    var popup = document.getElementById("myPopup3");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp4() {
+                    var popup = document.getElementById("myPopup4");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp5() {
+                    var popup = document.getElementById("myPopup5");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp6() {
+                    var popup = document.getElementById("myPopup6");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp7() {
+                    var popup = document.getElementById("myPopup7");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp8() {
+                    var popup = document.getElementById("myPopup8");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp9() {
+                    var popup = document.getElementById("myPopup9");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp10() {
+                    var popup = document.getElementById("myPopup10");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp11() {
+                    var popup = document.getElementById("myPopup11");
+                    popup.classList.toggle("show");
+                }
+
+                function popUp12() {
+                    var popup = document.getElementById("myPopup12");
+                    popup.classList.toggle("show");
+                }
