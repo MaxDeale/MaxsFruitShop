88a3d18faf31cab1217301fc700498695f9cf5d8
MaxDeale <46649047+MaxDeale@users.noreply.github.com>
2019-11-21 10:51:34 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ let cartCount = 0;
 // session storage load check
 
 function myLoad() {
-  if (sessionStorage.getItem("hasCodeRunBefore") === null) { // if page hasnt been loaded , set the array of cart items to session storage
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+    // if page hasnt been loaded , set the array of cart items to session storage
     sessionStorage.setItem("cart", JSON.stringify(cartItems));
+    sessionStorage.setItem("totals", total);
     sessionStorage.setItem("hasCodeRunBefore", true);
-  } else { // otherwise set the array to whatever is in the session storage
+  } else {
+    // otherwise set the array to whatever is in the session storage
     cartItems = JSON.parse(sessionStorage.getItem("cart"));
   }
 }
@@ -40,33 +43,27 @@ function decrementCart() {
 
 // ADD TO CART
 
-
-
-
 function addItem() {
   // Code to add event listeners to all of the "add to cart" buttons to get the correct information about the specific fruit clicked on
   let fruitinfo = [];
 
-  this.getinfo = function () {
+  this.getinfo = function() {
     let buttons = [document.querySelectorAll(".btn-primary")];
-    buttons.forEach(function () {
-
-
-      this.addEventListener("click", function (event) {
+    buttons.forEach(function() {
+      this.addEventListener("click", function(event) {
         var buttonCLicked = event.target;
-        var itemName = buttonCLicked.parentElement.firstChild.nextElementSibling.innerHTML;
-        var itemprice = buttonCLicked.parentElement.firstChild.nextElementSibling.nextElementSibling.firstChild.nextElementSibling.innerHTML;
+        var itemName =
+          buttonCLicked.parentElement.firstChild.nextElementSibling.innerHTML;
+        var itemprice =
+          buttonCLicked.parentElement.firstChild.nextElementSibling
+            .nextElementSibling.firstChild.nextElementSibling.innerHTML;
         fruitinfo.push(itemName);
         fruitinfo.push(itemprice);
-
       });
 
-      // return fruitinfo;
-
+      return fruitinfo;
     });
-  }
-
-
+  };
 
   // when a user clicks on "add to cart", the info from the html page must be sent as info to create an object using the constructor function
   // MUST FIND OUT HOW TO PUT IN VALUES FROM HTML WHEN CLICK ADD BUTTON
@@ -77,7 +74,7 @@ function addItem() {
   let couponOption = prompt("Would you like to use a -R10 coupon? (yes/no)");
   if (couponOption === "yes" || couponOption === "Yes") {
     // If the user uses a coupon , minus 10 from the total and use yes as the coupon info
-    total = (total - 10);
+    total = total - 10;
     useCoupon = "Yes";
   } else if (couponOption === "no" || couponOption === "No") {
     total = total;
@@ -85,44 +82,63 @@ function addItem() {
   } else {
     couponOption = prompt("please enter a valid option (yes/no)");
   }
-  console.log(total)
-  let deliveryOption = prompt("Enter 1 for overnight delivery(+R100), or 2 for Express Delivery(+R50)");
+  console.log(total);
+  let deliveryOption = prompt(
+    "Enter 1 for overnight delivery(+R100), or 2 for Express Delivery(+R50)"
+  );
   if ((deliveryOption = 1)) {
-    total = (total + 100);
+    total = total + 100;
     delivery = "Overnight";
   } else if ((deliveryOption = 2)) {
-    total = (total + 50);
+    total = total + 50;
     delivery = "Express";
   } else {
     deliveryOption = prompt("please enter a valid option (1 or 2)");
   }
 
   getinfo();
-  // NEED TO CONVERT THE FRUITIFNO OBJECT INTO AN ARRAY?
+  // NEED TO CONVERT THE FRUITIFNO OBJECT INTO AN ARRAY? CANT ACCESS VALUES
+
   console.log(fruitinfo);
 
+  // using dummy data for now
 
+  let newCartItem = new fruit("Raspberries", 26, useCoupon, delivery);
 
+  total = total + 26;
 
-
-
-
-  let newCartItem = new fruit(fruitinfo[0], fruitinfo[1], useCoupon, delivery);
-
-  total = (total + fruitinfo[1]);
+  sessionStorage.setItem("totals", total);
 
   // when add to cart is clicked, an alert should display the current total
-  alert("Your current total is R" + total + ". Please click on 'View Cart' to see your current cart items.");
+  alert(
+    "Your current total is R" +
+      total +
+      ". Please click on 'View Cart' to see your current cart items."
+  );
   // display a new row on bootstrap table for new shopping item
 
   var table = document.getElementById("tableInsert");
-
-  table.innerHTML += "<tr><th scope= " +
-    "'row'>" + cartCount + "</th>" + "<td>" +
-    newCartItem.fruitName + "</td>  " + " <td>" + newCartItem.fruitPrice +
-    "</td>" + " <td>" + newCartItem.fruitDelivery + "</td>" + "  <td>" +
-    newCartItem.fruitCoupon + "</td>" + " <td>< i onclick = 'removeItem()' " +
-    " class = 'fas fa-times' > < /i></td > " + "</tr>";
+  console.log(table);
+  table.innerHTML +=
+    "<tr><th scope= " +
+    "'row'>" +
+    cartCount +
+    "</th>" +
+    "<td>" +
+    newCartItem.fruitName +
+    "</td>  " +
+    " <td>" +
+    newCartItem.fruitPrice +
+    "</td>" +
+    " <td>" +
+    newCartItem.fruitDelivery +
+    "</td>" +
+    "  <td>" +
+    newCartItem.fruitCoupon +
+    "</td>" +
+    " <td>< i onclick = 'removeItem()' " +
+    " class = 'fas fa-times' > < /i></td > " +
+    "</tr>";
   // add the item into session storage
   cartItems.push(newCartItem);
   sessionStorage.setItem("cart", JSON.stringify(cartItems));
@@ -141,8 +157,21 @@ function removeItem() {
 // CHECKOUT
 
 function checkout() {
+  sessionStorage.setItem("totals", total);
   alert("Thank you, your order has been processed. Your Total is R" + total);
 }
+
+// JQUERY
+
+// $(document).ready(function () {
+
+//   $("body").on("load", function () {
+//     $("#main-nav").fadeIn(3000);
+
+//     $(".card").slideDown(1000);
+//   });
+
+// });
 
 // When the user clicks on <div>, open the popup
 function popUp() {
